65c0d33e1ff11ddd2bcefed59696c1e432f105b5
Mettfan <cesaryannick6@gmail.com>
2022-09-05 00:36:57 -0500

    'ddep96'

--- src/features/products/productSlicetest.js
+++ src/features/products/productSlicetest.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import back from "../back";
+
 export const productSlicetest = createSlice({
     name: 'productos',
     initialState: {
